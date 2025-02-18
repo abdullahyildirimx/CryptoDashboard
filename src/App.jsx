20d162bdbe5edfa363f3965909acbdeb0b784576
Abdullah Yıldırım <ayild1008@gmail.com>
2025-02-18 16:30:58 +0300

    Router 404 error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import MenuHeader from './components/MenuHeader';
 import MenuFooter from './components/MenuFooter';
 import useDocumentTitle from './hooks/useDocumentTitle';
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true,
     port: 3000
   }
 })
