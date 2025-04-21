33f34020b6b92e0f914534c90922c77283d399c9
Abdullah Yıldırım <ayild1008@gmail.com>
2025-04-21 08:04:50 +0300

    Pathname fixes

--- src/hooks/useDocumentTitle.js
+++ src/hooks/useDocumentTitle.js
@@ -1,18 +1,18 @@
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 const useDocumentTitle = () => {
-  const { coinData } = useSelector((state) => state.dataStore);
+  const { spotCoinData, futuresCoinData } = useSelector((state) => state.dataStore);
 
   useEffect(() => {
-
-    const btcPriceUsd = coinData?.find(item => item.symbol === "BTC").price;
+    const isSpot = location.pathname === '/spot';
+    const btcPriceUsd = isSpot ? spotCoinData?.find(item => item.symbol === "BTC").price : futuresCoinData?.find(item => item.symbol === "BTC").price;
     const title =
       btcPriceUsd
         ? `Dashboard - $${btcPriceUsd}`
         : 'Crypto Dashboard';
 
     document.title = `${title}`;
-  }, [coinData]);
+  }, [spotCoinData, futuresCoinData]);
 }
 
 export default useDocumentTitle;
