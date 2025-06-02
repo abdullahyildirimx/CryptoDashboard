8c94828e9774fc5c788969f678339cfcc2f4b7ed
Abdullah Yıldırım <ayild1008@gmail.com>
2025-06-02 22:14:56 +0300

    Domain change

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -6,8 +6,8 @@ export const balancesUrl = 'https://api.binance.com/api/v3/account'
 export const orderUrl = 'https://api.binance.com/api/v3/order'
 export const spotCoinLogosUrl = 'https://www.binance.com/bapi/composite/v1/public/marketing/symbol/list'
 export const futuresCoinLogosUrl = 'https://www.binance.com/bapi/asset/v1/public/asset/asset/get-asset-logo'
-export const spotMarketActivityUrl = 'https://api.cryptodashboards.xyz/spot'
-export const futuresMarketActivityUrl = 'https://api.cryptodashboards.xyz/futures'
+export const spotMarketActivityUrl = 'https://api.cryptodashboard.tech/spot'
+export const futuresMarketActivityUrl = 'https://api.cryptodashboard.tech/futures'
 export const getLogoLink = (number) => {
     return `https://s2.coinmarketcap.com/static/img/coins/64x64/${number}.png`;
 }
